0c6f9f59551d15df4c90f53bd849fd68faf9d74b
Jordi Böhme <jordi@boehme-lopez.de>
2026-03-30 13:22:42 +0200

    Update dependencies: Express 5, remove uuid for built-in crypto

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const { randomUUID } = require('crypto');
 const cache = require('./cache');
 const timestamps = require('./timestamps');
 
@@ -30,7 +30,7 @@ function savePhonebook(phonebook) {
 }
 
 function generateId() {
-  return uuidv4();
+  return randomUUID();
 }
 
 function getAllEntries() {
